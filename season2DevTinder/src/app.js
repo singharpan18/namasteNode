ab4604cc78f6eb00605c8b7a3c7d73af461b1d6f
arpansingh <arpansingh8583@gmail.com>
2024-09-23 12:18:28 +0530

    season2 lec3

---
+++ season2DevTinder/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+//create a sever
+//app.listen(console.log("Server is successfull running"));
+
+
+//write request handlers for /test, /hello
+app.use("/test", (req, res) => {
+    res.send("Hello from test server");
+});
+
+app.use("/hello", (req, res) => {
+    res.send("hello from hello server....");
+});
+
+//listen to port no 7777
+app.listen(7777, () => {
+    console.log("Server is successfully listening on port 7777......")
+});
