336ce4477a1bb21d2374ea2c20de1d81511d4446
arpansingh <arpansingh8583@gmail.com>
2024-09-26 13:55:53 +0530

    middleware

--- season2DevTinder/lec5/src/app.js
+++ season2DevTinder/lec5/src/app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 
 //app.use("/route", rH, [rH2, rH3], rH4, rh5);
-
-app.get(
+//multiple route-handlers
+/*app.get(
   "/user",
   (req, res, next) => {
     console.log("Handling the route user!!");
@@ -30,7 +30,30 @@ app.get(
     console.log("Handling the route user 5!!");
     res.send("5th Response!!");
   }
-);
+);*/
+
+//writing-auth-middleware -> instead of writing auth call before each api call we have created auth middleware and used it
+const {adminAuth, userAuth} = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("User Data Sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted a user");
+});
+
+//for login authentication is not required
+app.post("/user/login", (req, res) => {
+  res.send("User logged in successfully!!");
+});
+
+app.get("/user/data", userAuth, (req, res) => {
+  res.send("User data sent!!");
+});
+
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
