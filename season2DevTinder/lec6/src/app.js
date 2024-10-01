18c76e4c3e719cf3bcdfcd231a5fd2286e0fb2ff
arpansingh <arpansingh8583@gmail.com>
2024-10-01 10:36:17 +0530

    app listen after db connect and schema

--- season2DevTinder/lec6/src/app.js
+++ season2DevTinder/lec6/src/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
-require("./config/database");
+//require("./config/database");
 
+const connectDB = require("./config/database");
 const app = express();
-app.listen(7777, () => {
-  console.log("Server is successfully listening on port 7777...");
+
+connectDB().then(() => {
+    console.log("Database connection established....");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777...");
+    });    
+})
+.catch((err) => {
+    console.log("Database cannot be connected...");
 });
+
+// app.listen(7777, () => {
+//   console.log("Server is successfully listening on port 7777...");
+// });
 
--- season2DevTinder/lec6/src/config/database.js
+++ season2DevTinder/lec6/src/config/database.js
@@ -5,10 +5,20 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     await mongoose.connect("mongodb+srv://singharpan9748:FOzGAqlAsmk7gR5u@namastenode.zncny.mongodb.net/?retryWrites=true&w=majority&appName=namasteNode");
 };
+
+
 //connectDB function is getting called and it is returing promise and we are handling it using then
-connectDB().then(() => {
+/*connectDB().then(() => {
     console.log("Database connection established....");
 })
 .catch((err) => {
     console.log("Database cannot be connected...");
-});
+});*/
+
+module.exports = connectDB;
+//It looks like the issue in your code is that you're calling connectDB() when exporting it, which means the function is executed 
+//immediately when the module is imported. This is causing the promise to be resolved before it is actually used in your server.js 
+//or app.js file.
+//You should export the connectDB function itself, not the result of calling it.
+
+
