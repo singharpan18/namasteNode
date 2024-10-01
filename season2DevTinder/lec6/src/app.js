c2b27a4cc4bcd7c15a6f21f8cfefda52bdeacc64
arpansingh <arpansingh8583@gmail.com>
2024-10-01 11:25:04 +0530

    saving data in db

--- season2DevTinder/lec6/src/app.js
+++ season2DevTinder/lec6/src/app.js
@@ -2,7 +2,25 @@ const express = require("express");
 //require("./config/database");
 
 const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  //creating a new instance of the user model
+  const user = new User({
+    firstName: "ak",
+    lastName: "Singh",
+    emailId: "arpan@singh.com",
+    password: "Arpan@123"
+  });
+
+  try{
+    await user.save();
+    res.send("User added successfully!");
+  } catch (err){
+    res.status(400).send("Error saving the user" + err.message);
+  }
+})
 
 connectDB().then(() => {
     console.log("Database connection established....");
--- season2DevTinder/lec6/src/config/database.js
+++ season2DevTinder/lec6/src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 //it returns us a promise and tells us weather connection was established successfully or not, so we should use asyc wait so it wait until the response comes, once response comes handle it accordingly
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://singharpan9748:FOzGAqlAsmk7gR5u@namastenode.zncny.mongodb.net/?retryWrites=true&w=majority&appName=namasteNode");
+    await mongoose.connect("mongodb+srv://singharpan9748:FOzGAqlAsmk7gR5u@namastenode.zncny.mongodb.net/devTinder");
 };
 
 
