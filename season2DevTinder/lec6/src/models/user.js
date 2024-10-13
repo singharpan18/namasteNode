e08bf879ccb7decfba3fe94c1a89c275db7d933a
arpansingh <arpansingh8583@gmail.com>
2024-10-13 23:18:57 +0530

    exploring validator library

--- season2DevTinder/lec6/src/models/user.js
+++ season2DevTinder/lec6/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 //Create a userSchema & user Model
 const userSchema = new mongoose.Schema(
@@ -18,10 +19,20 @@ const userSchema = new mongoose.Schema(
         required: true,
         unique: true,
         trim: true,
+        validate(value){
+          if(!validator.isEmail(value)){
+            throw new Error("Invalid email address: " + value);
+          }
+        },
       },
       password: {
         type: String,
         required: true,
+        validate(value){
+          if(!validator.isStrongPassword(value)){
+            throw new Error("Enter a Strong Password: " + value);
+          }
+        },
       },
       age: {
         type: Number,
@@ -38,6 +49,11 @@ const userSchema = new mongoose.Schema(
       photoUrl: {
         type: String,
         default: "https://geographyandyou.com/images/user-profile.png",
+        validate(value) {
+          if (!validator.isURL(value)) {
+            throw new Error("Invalid Photo URL: " + value);
+          }
+        },  
       },
       about: {
         type: String,
