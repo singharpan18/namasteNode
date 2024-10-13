1d28ad2f55aa6f1fbcb34a0e95a0af0ecf0890f8
arpansingh <arpansingh8583@gmail.com>
2024-10-13 19:38:58 +0530

    schema validations

--- season2DevTinder/lec6/src/app.js
+++ season2DevTinder/lec6/src/app.js
@@ -79,11 +79,12 @@ app.patch("/user", async (req, res) => {
   try {
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
+      runValidators: true,
     });
     console.log(user);
     res.send("User updated successfully");
   } catch (err) {
-    res.status(400).send("Something went wrong ");
+    res.status(400).send("Update Failed: " + err.message);
   }
 });
 
--- season2DevTinder/lec6/src/models/user.js
+++ season2DevTinder/lec6/src/models/user.js
@@ -1,25 +1,56 @@
 const mongoose = require("mongoose");
 
 //Create a userSchema & user Model
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
         type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 50
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
       },
       password: {
         type: String,
+        required: true,
       },
       age: {
         type: Number,
+        min: 18,
       },
       gender: {
         type: String,
+        validate(value){
+          if(!["male", "female", "others"].includes(value)){
+            throw new Error("Gender data is not valid!");
+          }
+        }
       },
-});
+      photoUrl: {
+        type: String,
+        default: "https://geographyandyou.com/images/user-profile.png",
+      },
+      about: {
+        type: String,
+        default: "This is a default about of the user!",
+      },
+      skills: {
+        type: [String],
+      },
+  },
+  //by adding this object automatic createdate and updatedate time will get update in db
+  {
+    timestamps: true,
+  }
+);
     
 module.exports = mongoose.model("User", userSchema);
