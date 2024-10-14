0fc0f65a53f261574e692881d516c913c02bcf94
arpansingh <arpansingh8583@gmail.com>
2024-10-14 15:41:17 +0530

    logout+editprofileAPI

--- season2DevTinder/lec11/src/routes/auth.js
+++ season2DevTinder/lec11/src/routes/auth.js
@@ -49,7 +49,15 @@ authRouter.post("/login", async(req, res) => {
     }catch (err) {
       res.status(400).send("ERROR: " + err.message);
     }
-  })
+  });
+
+  authRouter.post("/logout", async (req, res) => {
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+    });
+    res.send("Logout Successful!!");
+  });
+  
  
 module.exports = authRouter; 
   
