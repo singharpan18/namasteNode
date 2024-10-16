9edb72795e1d5fbaaceb309dfed4e53b1cbbcc45
arpansingh <arpansingh8583@gmail.com>
2024-10-16 16:28:34 +0530

    get connection api

--- season2DevTinder/lec11/src/routes/user.js
+++ season2DevTinder/lec11/src/routes/user.js
@@ -4,6 +4,8 @@ const userRouter = express.Router();
 const { userAuthNew } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
 //get all the pending connection request for the loggedin user
 userRouter.get("/user/requests/received", userAuthNew, async(req, res) => {
     try{
@@ -14,7 +16,7 @@ userRouter.get("/user/requests/received", userAuthNew, async(req, res) => {
             status: "interested",
         }).populate(
             "fromUserId",
-            "firstName lastName photoUrl age gender about skills"
+            USER_SAFE_DATA
         )
 
         res.json({
@@ -23,6 +25,34 @@ userRouter.get("/user/requests/received", userAuthNew, async(req, res) => {
         });
     }catch(err){
         res.statusCode(400).send("ERROR: " + err.message);
+    }
+});
+
+userRouter.get("/user/connections", userAuthNew, async(req, res) => {
+    try{
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted"},
+                { fromUserId: loggedInUser._id, status: "accepted"},
+            ],
+        })
+        .populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA);
+
+        console.log(connectionRequests);
+
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+              return row.toUserId;
+            }
+            return row.fromUserId;
+          });
+
+          res.json({ data });
+    }catch(err){
+        res.status(400).send({ message: err.message });
     }
 });
 
