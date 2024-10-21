887a41680c9051d982032fb9bd07d513a7927de3
arpansingh <arpansingh8583@gmail.com>
2024-10-21 16:22:38 +0530

    routing

--- devTinder-web/README.md
+++ devTinder-web/README.md
@@ -4,4 +4,11 @@
 - Install tailwind css
 - Install Daisy UI
 - Add NavBar component to App.js
-- 
+- Create a NavBar.jsx separate Component file
+- Install react router dom
+- Create BrowserRouter > Routes > Route=/ Body > RouteChildren
+- Create an Outlet in your Body Component
+- Create a footer
+- Create a Login Page
+- Body NavBar Route=/ => Feed Route=/login => Login Route=/connetions => Connections Router=/profile => Profile
+
--- devTinder-web/src/App.jsx
+++ devTinder-web/src/App.jsx
@@ -1,46 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Body from './components/Body'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className="navbar bg-base-300">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">
-    🙋‍♀️DevTinder</a>
-  </div>
-  <div className="flex-none gap-2">
-    <div className="dropdown dropdown-end mx-5">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>   
-  </>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>}>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
